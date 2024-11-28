242b3fd966f43c056bda5647b6e9c21735c3b562
Luis Carlos <luis.mello@ufcat.edu.br>
2024-11-28 01:25:54 -0300

    Atualizado com a versão do SEI v4.0.12

--- scrape-processos.py
+++ scrape-processos.py
@@ -1,101 +1,126 @@
-# Importação de bibliotecas necessárias
-import csv
-from selenium import webdriver
+# Atualizado - SEI v4.0.12	
+
+from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import csv
+import os
+import chromedriver_autoinstaller
 import time
 
-# Configurações para executar o navegador em modo "headless" (sem interface gráfica)
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+# Instala o chromedriver automaticamente se necessário
+chromedriver_autoinstaller.install()
 
-# Inicialização do navegador Chrome
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+# Obtém o diretório do projeto
+diretorio_projeto = os.getcwd()
 
-# URL do SEI! desejado (insira a URL no campo vazio)
-url_sei = ""
-if not url_sei:
-    raise ValueError("A URL do SEI! não foi fornecida.")
+# Função que iniciar o navegador no modo headless (sem interface gráfica)
+def iniciar_chrome_headless():
+    opcoes_chrome = Options()
+    opcoes_chrome.add_argument("--headless")
+    opcoes_chrome.add_argument("--disable-gpu")
+    opcoes_chrome.add_argument("--no-sandbox")
+    opcoes_chrome.add_argument("--disable-dev-shm-usage")
+    opcoes_chrome.add_argument("--disable-webgl")
+    driver = webdriver.Chrome(options=opcoes_chrome)
+    return driver
 
-driver.get(url_sei)
+# Função que auxilia iniciar o navegador no modo GUI (com interface gráfica)
+def iniciar_chrome_gui():
+    driver = webdriver.Chrome()
+    return driver
 
-# Localização e preenchimento dos campos de usuário e senha
-campo_usuario = driver.find_element(By.ID, 'txtUsuario')
-campo_senha = driver.find_element(By.ID, 'pwdSenha')
+# Descomente a linha do modo que deseje rodar, comente a outra
+driver = iniciar_chrome_headless()  # Modo headless
+# driver = iniciar_chrome_gui()  # Modo GUI (padrão)
+driver.get("https://sei.ufcat.edu.br/sip/login.php?sigla_orgao_sistema=UFCAT&sigla_sistema=SEI") # Adicione o link do SEI desejado
+
+# Credenciais de login
 usuario = ''
 senha = ''
 
-if not usuario or not senha:
-    raise ValueError("Usuário e senha são obrigatórios.")
-    
+# Preenche os campos de login
+campo_usuario = driver.find_element(By.ID, "txtUsuario")
+campo_senha = driver.find_element(By.ID, "pwdSenha")
 campo_usuario.send_keys(usuario)
 campo_senha.send_keys(senha)
 
-# Clique no botão de login
-botao_login = driver.find_element(By.ID, 'sbmLogin')
+# Clica no botão de login
+botao_login = driver.find_element(By.ID, "sbmAcessar")
 botao_login.click()
 
-# Preenchimento do campo de pesquisa rápida e seleção da opção de pesquisa de processos
+# Realiza uma pesquisa rápida vazia
+# Utilizado para chegar rapidamente a página de pesquisa sem utilizar JS, visto que a pesquisa direta falha com Selenium
 campo_pesquisa_rapida = driver.find_element(By.ID, "txtPesquisaRapida")
 campo_pesquisa_rapida.send_keys("")
 campo_pesquisa_rapida.send_keys(Keys.RETURN)
-opcao_pesquisar_processos = driver.find_element(By.CSS_SELECTOR, "input#optProcessos")
-opcao_pesquisar_processos.click()
 
-# Interagir com o campo de unidade geradora
+# Marca a opção "Processos"
+# Utiliza JS para isso, pois existem limitações no Selenium atual quando lida com radio buttons
+processos_radio_button = driver.find_element(By.ID, "optProcessos")
+driver.execute_script("arguments[0].checked = true;", processos_radio_button)
+driver.execute_script("arguments[0].click();", processos_radio_button)
+
+# Preenche o campo de unidade geradora
 campo_unidade_geradora = driver.find_element(By.CSS_SELECTOR, "input#txtUnidade")
 campo_unidade_geradora.click()
-nome_unidade_geradora = ""
 
-if not nome_unidade_geradora:
-    raise ValueError("O nome da unidade geradora é obrigatório.")
+# Define o nome da unidade geradora
+nome_unidade_geradora = "SETI"
+ultimo_char = nome_unidade_geradora[-1]
 
-# Preenchimento gradual do campo de unidade geradora
+# Preenche o campo da unidade geradora char por char
 for char in nome_unidade_geradora:
     campo_unidade_geradora.send_keys(char)
-    time.sleep(0.5)
+    # Adiciona uma demora entre cada letra, assim não gera problemas na digitação
+    # Além disso, evita clicar em outra unidade com devido a demora ao carregar, para isso espera mais tempo no último char
+    time.sleep(1.5 if char == ultimo_char else 0.5)
 
-# Seleção da opção e pressionar teclas TAB
+# Seleciona a unidade geradora na lista de opções
 campo_unidade_geradora.send_keys(Keys.ARROW_DOWN)
 campo_unidade_geradora.send_keys(Keys.ENTER)
 campo_unidade_geradora.send_keys(Keys.TAB)
 
-# Clique no botão de pesquisa
+# Clica no botão de pesquisa
 botao_pesquisar = driver.find_element(By.ID, 'sbmPesquisar')
 botao_pesquisar.click()
 
-# Aguardar implicitamente por 5 segundos
+# Aguarda 5 segundos para garantir que os resultados sejam carregados
 driver.implicitly_wait(5)
 
-# Loop para extrair dados de todas as páginas
+# Define o nome do arquivo CSV
+# Utiliza o nome da unidade geradora definida em nome_unidade_geradora
+nome_csv = f"{nome_unidade_geradora}_processo.csv"
+
+# Loop para percorrer as páginas de resultados
 while True:
-    # Localizar elementos na página
-    divInfraAreaTelaD = driver.find_element(By.XPATH, '//*[@id="divInfraAreaTelaD"]')
-    conteudo = driver.find_element(By.XPATH, '//*[@id="conteudo"]')
-    tabelas = conteudo.find_elements(By.CLASS_NAME, "resultado")
-    nome_csv = nome_unidade_geradora + "_processo.csv"
-
-    # Escrever dados em um arquivo CSV
-    with open(nome_csv, mode='a', newline='') as arquivo:
-        escrever = csv.writer(arquivo)
-        for tabela in tabelas:
-            td_elemento = tabela.find_element(By.CLASS_NAME, "resTituloEsquerda")
-            texto = td_elemento.text
-            texto_formatado = texto[-20:]
-            escrever.writerow([texto_formatado])
-
     try:
-        # Tentar encontrar e clicar no link "Próxima"
-        next_link = driver.find_element(By.XPATH, '//span[@class="pequeno"]/a[contains(text(), "Próxima")]')
-        next_link.click()
+        # Localiza a área de resultados e os elementos de processo
+        div_infra_area = driver.find_element(By.XPATH, '//*[@id="divInfraAreaTelaD"]')
+        conteudo = driver.find_element(By.XPATH, '//*[@id="conteudo"]')
+
+        # Encontra todos os elementos de link com a classe 'protocoloNormal' que carrega o número do processo consigo
+        elementos_processos = conteudo.find_elements(By.XPATH, './/a[@class="protocoloNormal"]')
+
+        # Abre o arquivo CSV para adicionar os números dos processos
+        with open(nome_csv, mode='a', newline='') as arquivo:
+            escritor = csv.writer(arquivo)
+            for elemento_processo in elementos_processos:
+                numero_processo = elemento_processo.text.strip()
+                escritor.writerow([numero_processo])  # Escreve o número do processo no CSV
+
+        # Navega para a próxima página
+        link_proxima = driver.find_element(By.LINK_TEXT, "Próxima")
+        link_proxima.click()
+
     except NoSuchElementException:
-        # Se não houver mais páginas, encerrar o navegador e imprimir "Sucesso"
-        driver.quit()
-        print("Sucesso")
+        
+        # Caso não encontre mais elemento "Próxima" significa que não existe mais páginas de pesquisa
+        # Então infere o fim do processo
+        print("Todas as páginas foram processadas com sucesso.")
         break
 
-# Finalizar o programa
+driver.quit()
 exit()
