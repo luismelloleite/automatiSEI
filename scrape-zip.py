b093bf1555d6ffefe2f3a259aacb00070a663b0d
Luis Carlos <luis.mello@ufcat.edu.br>
2024-11-28 01:26:55 -0300

    Atualizado com a versão do SEI v4.0.12

--- scrape-zip.py
+++ scrape-zip.py
@@ -1,62 +1,70 @@
-# Importação de bibliotecas necessárias
+# Atualizado - SEI v4.0.12	
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import csv
 import os
-from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
 
-# Instalação automática do ChromeDriver usando webdriver_manager
-webdriver.Chrome(ChromeDriverManager().install())
+chromedriver_autoinstaller.install()
 
-# Obtém o diretório do projeto
 diretorio_projeto = os.getcwd()
 
 # Solicita ao usuário o nome da pasta para os downloads
-nome_pasta = input("Digite o nome da pasta para os downloads: ")
+nome_pasta = "SETI"
 
 # Verifica se o nome da pasta foi fornecido
 if not nome_pasta:
-    raise ValueError("O nome da pasta para downloads não foi fornecido.")
+    raise ValueError("A pasta de downloads não foi informada.")
 
-# Cria o caminho completo para o diretório de downloads
 diretorio_downloads = os.path.join(diretorio_projeto, nome_pasta)
-
-# Se o diretório de downloads não existir, cria-o
 if not os.path.exists(diretorio_downloads):
     os.makedirs(diretorio_downloads)
 
-# Configurações do Chrome para definir o diretório de downloads
-opcoes_chrome = Options()
-opcoes_chrome.add_experimental_option("prefs", {
-    "download.default_directory": diretorio_downloads
-})
+# Função para iniciar o navegador no modo headless (sem interface gráfica)
+def iniciar_chrome_headless():
+    opcoes_chrome = Options()
+    opcoes_chrome.add_argument("--headless")
+    opcoes_chrome.add_argument("--disable-gpu")
+    opcoes_chrome.add_argument("--no-sandbox")
+    opcoes_chrome.add_argument("--disable-dev-shm-usage")
+    opcoes_chrome.add_argument("--disable-webgl")
+    
+    # Configura o diretório de download
+    prefs = {"download.default_directory": diretorio_downloads}
+    opcoes_chrome.add_experimental_option("prefs", prefs)
 
-# Inicializa o navegador Chrome com as opções configuradas
-driver = webdriver.Chrome(options=opcoes_chrome)
-driver.get("http://www.sei.ufcat.edu.br")
+    driver = webdriver.Chrome(options=opcoes_chrome)
+    return driver
 
-# Localiza os campos de usuário e senha
+def iniciar_chrome_gui():
+    opcoes_chrome = Options()
+    
+    prefs = {"download.default_directory": diretorio_downloads}
+    opcoes_chrome.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(options=opcoes_chrome)
+    return driver
+
+# Descomente a linha do modo que deseja rodar, comente a outra
+# driver = iniciar_chrome_headless()  # Modo headless
+driver = iniciar_chrome_gui()  # Modo GUI (padrão)
+driver.get("https://sei.ufcat.edu.br/sip/login.php?sigla_orgao_sistema=UFCAT&sigla_sistema=SEI") # Adicione o link do SEI desejado
+
+# Credenciais de login
+usuario = ''
+senha = ''
+
 campo_usuario = driver.find_element(By.ID, "txtUsuario")
 campo_senha = driver.find_element(By.ID, "pwdSenha")
-
-# Informações de usuário e senha (a serem preenchidas)
-usuario = input("Digite o nome de usuário: ")
-senha = input("Digite a senha: ")
-
-# Verifica se usuário e senha foram fornecidos
-if not usuario or not senha:
-    raise ValueError("Usuário e senha são obrigatórios.")
-
-# Preenche os campos de usuário e senha
 campo_usuario.send_keys(usuario)
 campo_senha.send_keys(senha)
 
-# Clica no botão de login
-botao_login = driver.find_element(By.ID, "sbmLogin")
+botao_login = driver.find_element(By.ID, "sbmAcessar")
 botao_login.click()
 
 # Abre o arquivo CSV e lê os dados (ignora o cabeçalho)
@@ -78,8 +86,6 @@ with open(nome_pasta + '_processo.csv', 'r') as arquivo:
         campo_pesquisa_rapida.clear()
         campo_pesquisa_rapida.send_keys(numero_processo)
         campo_pesquisa_rapida.send_keys(Keys.RETURN)
-
-        # Aguarda implicitamente por 10 segundos
         driver.implicitly_wait(10)
 
         # Muda para o frame de visualização do processo
@@ -98,26 +104,14 @@ with open(nome_pasta + '_processo.csv', 'r') as arquivo:
         for elemento in elementos:
             outer_html = elemento.get_attribute("outerHTML")
             
-            # Verifica se o elemento possui o atributo 'procedimento_gerar_zip'
             if 'procedimento_gerar_zip' in outer_html:
-                # Clica no elemento
                 elemento.click()
-
-                # Aguarda a presença do botão 'btnGerar'
                 btn_gerar = WebDriverWait(driver, 30).until(
                     EC.presence_of_element_located((By.NAME, "btnGerar"))
                 )
-                
-                # Clica no botão 'btnGerar'
                 btn_gerar.click()
-                
-                # Sai do loop, pois já encontrou o elemento desejado
                 break
 
-        # Retorna ao conteúdo padrão fora do frame
         driver.switch_to.default_content()
 
-        # Localiza novamente o campo de pesquisa rápida para a próxima iteração
-
-# Encerra o navegador
 driver.quit()
